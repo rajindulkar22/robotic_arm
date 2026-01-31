4e881c1050ead54921dc01fc909a4927c2a2b34b
raj <rajindulkar7@gmail.com>
2026-01-31 20:45:41 +0100

    before format

--- src/manipulator_description/launch/gazebo.launch.py
+++ src/manipulator_description/launch/gazebo.launch.py
@@ -1,59 +1,66 @@
-from launch import LaunchDescription
-from pathlib import Path
-
-from launch_ros.actions import Node
-from launch.actions import DeclareLaunchArgument, SetEnvironmentVariable, IncludeLaunchDescription
-from launch_ros.parameter_descriptions import ParameterValue
-from launch.substitutions import Command, LaunchConfiguration
-from launch.launch_description_sources import PythonLaunchDescriptionSource
 import os
+from pathlib import Path
 from ament_index_python.packages import get_package_share_directory
 
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, SetEnvironmentVariable
+from launch.substitutions import Command, LaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+
+
 def generate_launch_description():
+    manipulator_description = get_package_share_directory("manipulator_description")
 
-    manipulator_description_dir = get_package_share_directory("manipulator_description")
-
-    model_arg = DeclareLaunchArgument(
-        name="model", 
-        default_value= os.path.
-        join(manipulator_description_dir, "urdf", "manipulator.urdf.xacro"),
-        description="Absolute path to robot urdf file"
-        )
-
-    gazebo_resource_path = SetEnvironmentVariable(
-        name = "GZ_SIM_RESOURCE_PATH",
-        value = str(Path(manipulator_description_dir).parent.resolve())
+    model_arg = DeclareLaunchArgument(name="model", default_value=os.path.join(
+                                        manipulator_description, "urdf", "manipulator.urdf.xacro"
+                                        ),
+                                      description="Absolute path to robot urdf file"
     )
 
-    robot_description = ParameterValue(Command(["xacro ", LaunchConfiguration("model")]), value_type=str)
+    gazebo_resource_path = SetEnvironmentVariable(
+        name="GZ_SIM_RESOURCE_PATH",
+        value=[
+            str(Path(manipulator_description).parent.resolve())
+            ]
+        )
     
-    robot_state_publisher = Node(
-        package = "robot_state_publisher",
-        executable= "robot_state_publisher",
-        parameters= [{
-            "robot_description": robot_description,
-            "use_sim_time": True,
-        }]
+    ros_distro = os.environ["ROS_DISTRO"]
+    is_ignition = "True" if ros_distro == "humble" else "False"
+
+    robot_description = ParameterValue(Command([
+            "xacro ",
+            LaunchConfiguration("model"),
+            " is_ignition:=",
+            is_ignition
+        ]),
+        value_type=str
+    )
+
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        parameters=[{"robot_description": robot_description,
+                     "use_sim_time": True}]
     )
 
     gazebo = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(get_package_share_directory("ros_gz_sim"),
-            "launch",
-            "gz_sim.launch.py")
-        ),
-        launch_arguments={
-            "gz_args": "-v 4 -r empty.sdf"
-        }.items()
-
-    )
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory("ros_gz_sim"), "launch"), "/gz_sim.launch.py"]),
+                launch_arguments=[
+                    ("gz_args", [" -v 4 -r empty.sdf"]
+                    )
+                ]
+             )
 
     gz_spawn_entity = Node(
-        package = "ros_gz_sim",
-        executable = "create",
-        output = "screen",
-        arguments = ["-topic", "robot_description",
-                     "-name", "manipulator"]
+        package="ros_gz_sim",
+        executable="create",
+        output="screen",
+        arguments=["-topic", "robot_description",
+                   "-name", "manipulator"],
     )
 
     gz_ros2_bridge = Node(
@@ -61,18 +68,13 @@ def generate_launch_description():
         executable="parameter_bridge",
         arguments=[
             "/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock",
-            "/image_raw@sensor_msgs/msg/Image[gz.msgs.Image",
-            "/image_raw/camera_info@sensor_msgs/msg/CameraInfo[gz.msgs.CameraInfo",
-        ],
-        output="screen"
-
-
+        ]
     )
 
     return LaunchDescription([
         model_arg,
         gazebo_resource_path,
-        robot_state_publisher,
+        robot_state_publisher_node,
         gazebo,
         gz_spawn_entity,
         gz_ros2_bridge
